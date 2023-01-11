f4e4e7247add2f310a387caf55e29a60e776fa81
username <email@blahblah.comgit config --global user.name bogeystylegit config --global user.email chucklenati@gmail.com>
2023-01-11 10:17:32 -0500

    Initial Commit

---
+++ X Schedule 2 GitHub/X_Schedule_2_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  X_Schedule_2_GitHubApp.swift
+//  X Schedule 2 GitHub
+//
+//  Created by Stautberg, Charles on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct X_Schedule_2_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ X Schedule 2 GitHubUITests/X_Schedule_2_GitHubUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  X_Schedule_2_GitHubUITestsLaunchTests.swift
+//  X Schedule 2 GitHubUITests
+//
+//  Created by Stautberg, Charles on 1/11/23.
+//
+
+import XCTest
+
+final class X_Schedule_2_GitHubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
